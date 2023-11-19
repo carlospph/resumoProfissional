7108c7556037c8cfc373be435213ee82bcfd7bc7
carlospph <intablete@gmail.com>
2023-11-19 20:11:58 -0300

    Apresentação de produtos simples

--- Exercicio-show-produto/show.js
+++ Exercicio-show-produto/show.js
@@ -1,3 +1,36 @@
-function showProduto(){
-    alert("teste");
+function abrirElemento(){
+    // alert("teste");
+    let img = document.getElementById('imagem');
+    img.src='images/tamborete.png';
+    
 }
+
+function abrirElementoB(){
+    // alert("teste");
+    let imgb = document.getElementById('imagem');
+    imgb.src='images/poltronaAconchegante.png';
+}
+
+function abrirElementoC(){
+    // alert("teste");
+    let imgc = document.getElementById('imagem');
+    imgc.src='images/poltrona2.png';
+}
+
+function abrirElementoD(){
+    // alert("teste");
+    let imgd = document.getElementById('imagem');
+    imgd.src='images/class.png';
+}
+
+function abrirElementoE(){
+    // alert("teste");
+    let imge = document.getElementById('imagem');
+    imge.src='images/vovo.png';
+}
+
+
+
+
+
+
